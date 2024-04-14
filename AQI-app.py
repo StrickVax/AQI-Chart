3f252f89cf88cacf1554c231c9634e5e530afad6
StrickVax <strickvax@gmail.com>
2024-04-14 03:55:41 -0700

    Add lazy loading

--- AQI-app.py
+++ AQI-app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Output, Input
 import pandas as pd
 import plotly.express as px
 
@@ -10,37 +10,43 @@ with open("counties.json") as f:
 
 df = pd.read_csv("aqi_data.csv", dtype={"FIPS": str})
 
-fig = px.choropleth(
-    df,
-    geojson=counties,
-    locations="FIPS",
-    color="Median.AQI",
-    color_continuous_scale="Magma",
-    range_color=(0, 170),
-    scope="usa",
-    labels={"Median.AQI": "AQI Level"},
-)
-
-fig.update_layout(
-    margin={"r": 0, "t": 0, "l": 0, "b": 0},
-    paper_bgcolor="LightSteelBlue",
-)
-
 app = Dash(__name__)
 
 app.layout = html.Div(
     [
+        dcc.Graph(id="choropleth-map"),
         dcc.Slider(
             id="year-slider",
-            min=1980,
-            max=2023,
-            value=2023,
-            marks={str(year): str(year) for year in df["Year"]},
+            min=df["Year"].min(),
+            max=df["Year"].max(),
+            value=df["Year"].min(),
+            marks={str(year): str(year) for year in df["Year"].unique()},
             step=None,
-        ),
-        dcc.Graph(figure=fig),
+        )
     ]
 )
+
+
+@app.callback(Output("choropleth-map", "figure"), [Input("year-slider", "value")])
+def update_map(selected_year):
+    filtered_df = df[df["Year"] == selected_year]
+
+    fig = px.choropleth(
+        filtered_df,
+        geojson=counties,
+        locations="FIPS",
+        color="Median.AQI",
+        color_continuous_scale="Magma",
+        range_color=(0, 170),
+        scope="usa",
+        labels={"Median.AQI": "AQI Level"},
+    )
+
+    fig.update_layout(transition_duration=500)
+
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig
+
 
 if __name__ == "__main__":
     app.run(debug=False)
