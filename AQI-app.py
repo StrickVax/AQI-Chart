459c8af4b46ec8b909ddd15bd741576744cbcceb
StrickVax <strickvax@gmail.com>
2024-04-14 00:17:12 -0700

    Add choropleth

--- AQI-app.py
+++ AQI-app.py
@@ -1,13 +1,31 @@
-from dash import Dash, html, dash_table
+from dash import Dash, dcc, html
 import pandas as pd
+import plotly.express as px
 
-df = pd.read_csv('aqi_data.csv')
+# Load the county boundary coordinates
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+df = pd.read_csv('aqi_data.csv', dtype={"FIPS": str})
+
+fig = px.choropleth(df,
+                    geojson=counties,
+                    locations='FIPS',
+                    color='Median.AQI',
+                    color_continuous_scale="Magma",
+                    range_color=(0, 170),
+                    scope="usa",
+                    labels={'Median.AQI': 'AQI Level'}
+                    )
+
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(children='uh'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=1000)
+    dcc.Graph(figure = fig)
 ])
 
 if __name__ == '__main__':
