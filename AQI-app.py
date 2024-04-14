8f4fd7d5173a7a8cf7e56926ec9bd91fc3f96b26
StrickVax <strickvax@gmail.com>
2024-04-14 02:17:27 -0700

    Add year slider

--- AQI-app.py
+++ AQI-app.py
@@ -4,28 +4,43 @@ import plotly.express as px
 
 # Load the county boundary coordinates
 import json
-with open('counties.json') as f:
-        counties = json.load(f)
 
-df = pd.read_csv('aqi_data.csv', dtype={"FIPS": str})
+with open("counties.json") as f:
+    counties = json.load(f)
 
-fig = px.choropleth(df,
-                    geojson=counties,
-                    locations='FIPS',
-                    color='Median.AQI',
-                    color_continuous_scale="Magma",
-                    range_color=(0, 170),
-                    scope="usa",
-                    labels={'Median.AQI': 'AQI Level'}
-                    )
+df = pd.read_csv("aqi_data.csv", dtype={"FIPS": str})
 
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig = px.choropleth(
+    df,
+    geojson=counties,
+    locations="FIPS",
+    color="Median.AQI",
+    color_continuous_scale="Magma",
+    range_color=(0, 170),
+    scope="usa",
+    labels={"Median.AQI": "AQI Level"},
+)
+
+fig.update_layout(
+    margin={"r": 0, "t": 0, "l": 0, "b": 0},
+    paper_bgcolor="LightSteelBlue",
+)
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    dcc.Graph(figure = fig)
-])
+app.layout = html.Div(
+    [
+        dcc.Slider(
+            id="year-slider",
+            min=1980,
+            max=2023,
+            value=2023,
+            marks={str(year): str(year) for year in df["Year"]},
+            step=None,
+        ),
+        dcc.Graph(figure=fig),
+    ]
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=False)
